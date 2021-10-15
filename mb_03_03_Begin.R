9c4d13fc7b6d70c1ac46b9861ec87e4f73185ff0
MICHAEL BARADAS <15837639+mbaradas@users.noreply.github.com>
2021-10-15 15:29:56 -0400

    Add files via upload

---
+++ mb_03_03_Begin.R
@@ -0,0 +1,18 @@
+#03_03_Begin
+#Installing Quantmod
+#install.packages("quantmod")
+if(require("quantmod",quietly=TRUE))
+  install.packages("quantmod",dependencies=TRUE,repos=c(CRAN="http://cran.rstudio.com"))
+library("quantmod")
+getSymbols("AAPL")
+barChart(AAPL)
+barChart(AAPL, subset = 'last 28 days')
+rm(AAPL)
+getSymbols("MSFT")
+barChart(MSFT)
+chartSeries(MSFT)
+addMACD()
+addBBands()
+chartSeries(MSFT, subset = 'last 60 days')
+addMACD()
+addBBands()
---
+++ mb_03_05_Begin.R
@@ -0,0 +1,10 @@
+#03_05
+summary(X03_05_Start_R)
+linearMod<- lm(Cash ~ EBITDA, data = X03_05_Start_R)
+print(linearMod)
+
+
+
+
+
+
